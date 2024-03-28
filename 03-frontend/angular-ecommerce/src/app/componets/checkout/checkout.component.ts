6261953d017cebd58facb0a9d8a7f926f730aaaa
Arleta Madej <arleta.madej@gmail.com>
2024-03-28 20:25:52 +0100

    billing adress

--- 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
@@ -18,8 +18,45 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipcode: ['']
+      }),
+
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipcode: ['']
+      }),
+
+      creditCardInformation: this.formBuilder.group({
+        cartType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
+
     });
+  }
+
+  copyShippingAdressToBillingAdress(event: any){
+
+    if(event.target.checked){
+      this.checkoutFormGroup.controls['billingAddress']
+          .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else{
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
   }
 
   onSubmit(){
