848b4957f7d058f86f69775c7aace16fb0fdd5ee
Arleta Madej <arleta.madej@gmail.com>
2024-03-28 20:41:46 +0100

    cd

--- 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQunatity: number = 0;
 
   constructor(private formBuilder: FormBuilder) { }
 
