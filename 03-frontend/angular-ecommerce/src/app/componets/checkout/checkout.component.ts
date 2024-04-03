7050226bd383620cf731530d1b2975af1b3e8f31
Arleta Madej <arleta.madej@gmail.com>
2024-04-03 15:10:39 +0200

    populate countries and states

--- 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -15,6 +17,11 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears: number[] =[];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
 
   constructor(private formBuilder: FormBuilder, 
@@ -74,6 +81,22 @@ export class CheckoutComponent implements OnInit {
         }
       );
 
+      //populate countries
+      this.Luv2ShopFormService.getCountries().subscribe(
+        data => {
+          console.log("Retrieved cuntries: " + JSON.stringify(data));
+          this.countries = data;
+        }
+      );
+
+      //populate states
+      // this.Luv2ShopFormService.getStates().subscribe(
+      //   data => {
+      //     console.log("Retrieved cuntries: " + JSON.stringify(data));
+      //     this.countries = data;
+      //   }
+      // );
+
 
   }
 
@@ -117,6 +140,33 @@ export class CheckoutComponent implements OnInit {
       this.creditCardMonths = data;
     }
   );
+}
+
+getStates(formGroupName: string){
+
+  const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+  const countryCode = formGroup?.value.country.code;
+  const countryName = formGroup?.value.country.name;
+
+  console.log(`{formGroupName} country code: ${countryCode}`);
+  console.log(`{formGroupName} country name: ${countryName}`);
+
+  this.Luv2ShopFormService.getStates(countryCode).subscribe(
+    data => {
+
+      if(formGroupName === 'shippingAddress'){
+        this.shippingAddressStates = data;
+      }
+      else {
+        this.billingAddressStates = data;
+      }
+
+      //select the first item as default
+      formGroup?.get('state')?.setValue(data[0]);
+      
+    }
+  )
 }
 
 }
