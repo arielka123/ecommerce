385f5f8b800faba1be3d4619dbab91918bc69884
Arleta Madej <arleta.madej@gmail.com>
2024-04-03 18:00:26 +0200

    added validators

--- 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/componets/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
@@ -31,9 +31,9 @@ export class CheckoutComponent implements OnInit {
     this.checkoutFormGroup = this.formBuilder.group({
 
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
 
       shippingAddress: this.formBuilder.group({
@@ -88,17 +88,13 @@ export class CheckoutComponent implements OnInit {
           this.countries = data;
         }
       );
-
-      //populate states
-      // this.Luv2ShopFormService.getStates().subscribe(
-      //   data => {
-      //     console.log("Retrieved cuntries: " + JSON.stringify(data));
-      //     this.countries = data;
-      //   }
-      // );
-
-
   }
+
+
+  get firstName(){ return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName(){ return this.checkoutFormGroup.get('customer.lastName'); }
+  get email(){ return this.checkoutFormGroup.get('customer.email'); }
+
 
   copyShippingAdressToBillingAdress(event: any){
 
@@ -120,11 +116,18 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit(){
     console.log("Handlink the submit button");
+
+    if(this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
+
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is  " + this.checkoutFormGroup.get('customer')?.value.email);
 
     console.log("The shipping address country is  " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
     console.log("The shipping address state is  " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+
 
   }
 
