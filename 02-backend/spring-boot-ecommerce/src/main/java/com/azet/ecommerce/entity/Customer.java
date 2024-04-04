03c3dfb4bb48e45c82395e10fcf3a9e29e439879
Arleta Madej <arleta.madej@gmail.com>
2024-04-04 18:54:47 +0200

    added new entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/azet/ecommerce/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.azet.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="customer")
+@Getter
+@Setter
+class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+}
