9112954f69c0a7c526345cbef3108293b6d92481
Arleta Madej <arleta.madej@gmail.com>
2024-04-14 20:28:13 +0200

    created login-component

--- 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from 'src/app/config/my-app-config';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  oktaSignin: any;
+  
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+   }
 
   ngOnInit(): void {
   }
