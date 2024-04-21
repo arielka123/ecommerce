5cf87ca76b446bb738b0e2ca2581b98468c39de9
Arleta Madej <arleta.madej@gmail.com>
2024-04-21 15:13:43 +0200

    OrderHistoryComponent

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -28,6 +28,7 @@ import{
 import { OktaAuth } from '@okta/okta-auth-js';
 import  myAppConfig from 'src/app/config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -72,7 +73,8 @@ const routes: Routes= [
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
