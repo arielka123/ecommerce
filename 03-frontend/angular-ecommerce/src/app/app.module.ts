e2836ac2e5b66ad526fc42ba3935310f14473658
Arleta Madej <arleta.madej@gmail.com>
2024-04-21 15:38:43 +0200

    updated html

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -46,6 +46,9 @@ const routes: Routes= [
   {path:'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
                     data: {onAuthRequired: sendToLoginPage }},
 
+  {path:'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard],
+                  data: {onAuthRequired: sendToLoginPage }},
+
   {path:'login/callback', component: OktaCallbackComponent},
   {path:'login', component: LoginComponent},
 
